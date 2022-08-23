b2611fde00f32371495b28591acece8a153e7d02
dmiszczuk <Dawid.Miszczuk@dataart.com>
2022-08-23 11:48:26 +0200

    Initial commit

---
+++ src/Empty.java
@@ -0,0 +1,40 @@
+import MyPApplet.MyPApplet;
+import processing.core.*;
+
+public class Empty extends MyPApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("Empty");
+    }
+
+    @Override
+    public void settings() {
+        //fullScreen();
+        size(200, 200);//2340);//1280);
+        this.poh = this.displayHeight / 90f; // height unit
+        //size(displayWidth, displayHeight);
+    }
+
+    @Override
+    public void setup() {
+        rectMode(CORNERS);
+        //size(1080, 1000);//2340);//1280);
+        fill(255);
+        frameRate(60);
+        background(0);
+        textAlign(LEFT);
+
+        surface.setTitle("Empty project");
+        //surface.setResizable(false);
+        surface.setLocation(this.displayWidth / 2 - this.sketchWidth()/2, this.displayHeight / 4);
+    }
+
+    @Override
+    public void draw() {
+        background(0);
+
+        text("Hello World!", mouseX, mouseY);
+
+    }
+
+}
---
+++ src/MyPApplet/MyPApplet.java
@@ -0,0 +1,9 @@
+package MyPApplet;
+
+public class MyPApplet extends processing.core.PApplet {
+
+    public Float poh = 0f;
+
+    public MyPApplet(){}
+
+}
